35d4d48dc33b60c04ca2bd149ca8e392558806b7
Jerry Fitzner <jfitzner1@gmail.com>
2021-12-22 12:33:57 -0600

    Completted assignment

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function introduction(name) {
+    return ("Hi, my name is " + name + '.');
+}
+
+function introductionWithLanguage(name,language) {
+    return('Hi, my name is ' + name + ' and I am learning to program in ' + language + '.');
+} 
+
+let language = JavaScript;
+
+function introductionWithLanguageOptional(name,language = "JavaScript"){
+    return(`Hi, my name is ${ name } and I am learning to program in ${ language }.`);
+}
+
+
+introduction(Aki);
+introductionWithLanguage(Aki,Ember.js);
+introductionWithLanguageOptional(Gracie);
+introductionWithLanguageOptional(Gracie,Python);
